cbbd8404b08b9d9d1cbba6d6bade8e2e0d2d0b80
Angel Coco Chen <angel@nuisance.co>
2018-03-22 18:15:51 +0800

    Replace mock data with query and data from server

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
 import Link from './Link'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'Prisma turns your database into a GraphQL API 😎 😎',
-        url: 'https://www.prismagraphql.com',
-      },
-      {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://www.apollographql.com/docs/react/',
-      },
-    ]
+    // 1
+    if (this.props.feedQuery && this.props.feedQuery.loading) {
+      return <div>Loading</div>
+    }
+
+    // 2
+    if (this.props.feedQuery && this.props.feedQuery.error) {
+      return <div>Error</div>
+    }
+
+    // 3
+    const linksToRender = this.props.feedQuery.feed.links
 
     return (
       <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
@@ -22,4 +24,20 @@ class LinkList extends Component {
   }
 }
 
-export default LinkList
+const FEED_QUERY = gql`
+  query FeedQuery {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`
+
+// graphql is a container
+// the options object is what Apollo uses to inject props into the wrapped component
+// if no name is provided, the injected prop is called "data" by default
+export default graphql(FEED_QUERY, { name: 'feedQuery' })(LinkList)
