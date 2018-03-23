0d81e3c67d82e5cd412f3188d68048417e24a024
Angel Coco Chen <angel@nuisance.co>
2018-03-23 11:42:26 +0800

    Added routing and navigation, redirect to LinkList after CreateLink

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
 
 // HttpLink connects ApolloClient instance with the GraphQL API
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
@@ -18,9 +19,11 @@ const client = new ApolloClient({
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root'),
 )
 registerServiceWorker()
